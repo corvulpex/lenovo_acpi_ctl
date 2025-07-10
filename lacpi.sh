49d5df407640b37f1298375cdc2f4f17622e3213
corvulpex <mailnotfound>
2025-07-11 01:14:03 +0200

    remade use of command

--- lacpi.sh
+++ lacpi.sh
@@ -1,40 +1,56 @@
 #! /usr/bin/bash
 
-if (( $# > 1 )) then 
-	echo "Unsupported number of arguments"
-	echo $#
+if [ $# == 0 ]; then 
+	echo "No command specified"
 	exit 1
 fi
 
-if [ $# == 0 ]; then 
-	old_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
-	target_mode=$((($old_mode + 1) % 2))
-	echo $target_mode > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode 
-	new_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
-	target_string=$([ "$target_mode" == 0 ] && echo "down" || echo "up")
-	if [ $new_mode != $target_mode ]; then 
-		echo "Could not set mode to ${target_string}"
-		exit 1
+battery_conservation_control () {
+	
+	if [[ $# == 1 ]]; then 
+		old_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
+		target_mode=$((($old_mode + 1) % 2))
+		echo $target_mode > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode 
+		new_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
+		target_string=$([ "$target_mode" == 0 ] && echo "down" || echo "up")
+		if [ $new_mode != $target_mode ]; then 
+			echo "Could not set battery conservation to ${target_string}"
+			exit 1
+		fi
+		echo "Battery conservation set to ${target_string}"
+		return
 	fi
-	echo "Mode set to ${target_string}"
+
+	if [[ $2 == "down" ]]; then 
+		echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode 
+		new_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
+		if [ $new_mode != 0 ]; then
+			echo "Could not set battery conservation to down"
+		else 
+			echo "Battery conservation set to down"
+		fi
+		return
+	fi
+	
+	if [[ $2 == "up" ]]; then 
+		echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode 
+		new_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
+		if [ $new_mode != 1 ]; then
+			echo "Could not set battery conservation to up"
+		else 
+			echo "Battery conservation set to up"
+		fi
+		return	
+	fi
+
+	echo "Options for battery conservation control could not be interpreted correctly"
+	exit 1
+}
+
+if [[ $1 == "bcm" ]]; then 
+	battery_conservation_control $@
 	exit 0
 fi
 
-if [ $1 == "down" ]; then 
-	echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode 
-	new_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
-	if [ $new_mode != 0 ]; then
-		echo "Could not set mode to down"
-	else 
-		echo "Mode set to down"
-	fi
-elif [ $1 == "up" ]; then 
-	echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode 
-	new_mode=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
-	if [ $new_mode != 1 ]; then
-		echo "Could not set mode to up"
-	else 
-		echo "Mode set to up"
-	fi
-fi
-
+echo "No commands except bcm supported yet"
+exit 1
